9cbdfdde6a9772568751822f6afa28a7daa760d9
pvox dev <abdulrehmandev388@gmail.com>
2022-07-05 19:33:59 +0200

    deploy container app after invalidation cache

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/FirstLayoutApp";
 
 export default () => {
-  console.log("container app");
+  console.log("container app after invalidation cache");
   return (
     <div>
       <MarketingApp />
