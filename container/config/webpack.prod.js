13781c6bc4cb45083930ce1b3e09e5744aa134c1
pvox dev <abdulrehmandev388@gmail.com>
2022-07-05 02:35:57 +0200

    create container deploy

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,20 +1,20 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: '[name].[contenthash].js',
+    filename: "[name].[contenthash].js",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        firstLayout: `firstLayout@${domain}/firstlayout/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
