82a6b5591c71df5362d75db4fd192b940bcc4284
pvox dev <abdulrehmandev388@gmail.com>
2022-07-05 17:10:10 +0200

    deploy container

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/FirstLayoutApp";
 
 export default () => {
-  console.log("hghtgjg");
+  console.log("container app");
   return (
     <div>
       <MarketingApp />
