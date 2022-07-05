a315b04acab13d48aa66da3e8ab5fb681d713b41
pvox dev <abdulrehmandev388@gmail.com>
2022-07-05 19:48:16 +0200

    deploy container app an first-layout

--- first_layout/src/pages/index.js
+++ first_layout/src/pages/index.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import '../MuiClassNameSetup';
+import React from "react";
+import "../MuiClassNameSetup";
 import "./style.css";
-import Home from "./Main"
-import ProductDetails from "./product"
-import {
-    BrowserRouter as Router,
-    Routes ,
-    Route,
-    Link
-  } from "react-router-dom";
+import Home from "./Main";
+import ProductDetails from "./product";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function index() {
-   
+  console.log("first");
   return (
     <>
-     <Router>
-     <Routes >
-       <Route path="/"  element={<Home/>}/>
-       <Route path="/product" element={<ProductDetails/>}/>
-     </Routes >
-</Router> 
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<ProductDetails />} />
+        </Routes>
+      </Router>
     </>
-   )
+  );
 }
 
-export default index
+export default index;
