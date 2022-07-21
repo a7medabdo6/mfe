8d0503f546748a9571506df02e426876c2bb0932
pvox dev <abdulrehmandev388@gmail.com>
2022-07-21 20:26:29 +0200

    add image

--- first_layout/src/pages/Main.js
+++ first_layout/src/pages/Main.js
@@ -6,6 +6,24 @@ import NewsLetter from "../components/NewsLetter";
 function Main() {
   return (
     <>
+      <div
+        style={{
+          width: "100%",
+          height: "250px",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "150px",
+            backgroundImage:
+              "url('https://okmenuqr.com/_nuxt/img/restaurant.f3a09ab.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "50%",
+          }}
+        ></div>
+      </div>
+
       <Tabscom />
       <NewsLetter />
     </>
